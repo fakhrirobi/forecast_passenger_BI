92af7233aa21e064db18f95423dcde208b09829b
Fakhri Robi Aulia <fakhrirobi.fra@gmail.com>
2022-01-23 14:44:08 +0700

    adding path changer with contextmanager

--- src/data/datapreparation/generate_modified_data.py
+++ src/data/datapreparation/generate_modified_data.py
@@ -2,14 +2,23 @@ import prepare_data
 
 # importing module
 import logging
- 
+from contextlib import contextmanager
 # Create and configure logger
 logging.basicConfig(filename="newfile.log",
                     format='%(asctime)s %(message)s',
                     filemode='w')
 
 logger = logging.getLogger()
-
+@contextmanager
+def change_path() : 
+    prev_cwd = os.get_cwd()
+    
+    try : 
+        os.chdir('..')
+    finally : 
+        os.chdir(prev_cwd) 
+        
+    
 
 def begin_process() : 
     wrangler = prepare_data.DataWrangler()
@@ -18,9 +27,10 @@ def begin_process() :
     data = wrangler.load_data()
 
     eda_data = wrangler.prepare_eda_data(data)
-
-    eda_data.to_csv('prepare_for_eda.csv')
+    with change_path() : 
+        eda_data.to_csv('prepare_for_eda.csv',index=False)
     print(eda_data.head(4))
+
     logger.info("Finished preparing data for EDA")
 
 
@@ -29,7 +39,8 @@ def begin_process() :
     # the output should contain two columns : Activity Period and Passengger Total 
     forecast_data = wrangler.prepare_forecast_data(eda_data)
     print(forecast_data.head(4))
-    forecast_data.to_csv('forecast_data.csv',index=False)
+    with change_path() : 
+        forecast_data.to_csv('forecast_data.csv',index=False)
     logger.info("Finished preparing data for forecast data")
 if __name__ == '__main__' : 
     begin_process()
